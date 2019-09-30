08cd50fa9cc39b10b1e7ee049e6bbaf8a146ffb7
Eugene Leto <mholy@protonmail.ch>
2019-09-30 15:30:15 +0500

    move routes to bloglist controller

--- index.js
+++ index.js
@@ -4,24 +4,12 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const Blog = require('./models/blog')
+const bloglistRouter = require('./controllers/bloglist')
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then(blogs => {
-        response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog.save().then(result => {
-        response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', bloglistRouter)
 
 app.listen(config.PORT, () => {
     console.log(`Server running on port ${config.PORT}`)
