962715b7d2865e18fb537f3e39bd8ec6105702a6
Eugene Leto <mholy@protonmail.ch>
2019-10-01 20:40:48 +0500

    Exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,14 @@
 const dummy = blogs => 1
 
+const totalLikes = blogs => {
+    return blogs
+        .map(blog => blog.likes)
+        .reduce((acc, curr) => {
+            return acc + curr
+        }, 0)
+}
+
 module.exports = {
     dummy,
+    totalLikes,
 }
