66145f3cbb6fd7abe1c314bd30c38abaf0213b2d
Eugene Leto <mholy@protonmail.ch>
2019-10-10 23:32:03 +0500

    api tests for check that app returns json and correct amount of entries

---
+++ tests/bloglist_api.test.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const Blog = require('../models/blog')
+const app = require('../app')
+const supertest = require('supertest')
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+    },
+    {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url:
+            'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+    },
+    {
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+    },
+]
+
+beforeAll(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+
+    await Promise.all(promiseArray)
+})
+
+test('bloglist entries are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('app returns correct amount of blog entries', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(initialBlogs.length)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
